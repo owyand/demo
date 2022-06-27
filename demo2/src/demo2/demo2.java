782abde77f03da6193d39e09b5176cd7523707c6
owyand <oliviawyand@gmail.com>
2022-06-27 13:10:51 -0400

    added demo2.java class

---
+++ demo2/src/demo2/demo2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package demo2;
+
+/**
+ * @author Oliver Wyand
+ *
+ */
+public class demo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("hello gtihub");
+
+	}
+
+}
