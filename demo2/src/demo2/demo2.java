676cf850208173e910d7b9a84120bafef62e3a34
owyand <oliverwyand@gmail.com>
2022-06-27 13:51:53 -0400

    practicing branching pulling and merging

--- demo2/src/demo2/demo2.java
+++ demo2/src/demo2/demo2.java
@@ -16,6 +16,8 @@ public class demo2 {
 
 		System.out.println("hello github");
 		System.out.println("this line was added in a branch created from local IDE");
+		
+		System.out.println("practicing branching pull requests and merging");
 
 	}
 
